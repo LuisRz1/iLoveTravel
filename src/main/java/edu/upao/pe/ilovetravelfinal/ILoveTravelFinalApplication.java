c4c7f987ee31b415f8e00cbebaa4503fd71835fc
LuisRz1 <langulor1@upao.edu.pe>
2023-11-04 12:13:01 -0500

    first commit

---
+++ src/main/java/edu/upao/pe/ilovetravelfinal/ILoveTravelFinalApplication.java
@@ -0,0 +1,13 @@
+package edu.upao.pe.ilovetravelfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveTravelFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILoveTravelFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/upao/pe/ilovetravelfinal/ILoveTravelFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upao.pe.ilovetravelfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILoveTravelFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
