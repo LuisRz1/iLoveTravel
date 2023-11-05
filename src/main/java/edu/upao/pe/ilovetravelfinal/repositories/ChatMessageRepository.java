76ec28ce67535ea97a4fe26ce821c16b78c1b994
Daniel R <drodrigueza10@upao.edu.pe>
2023-11-05 17:27:35 -0500

    "ChatMessage"

--- src/main/java/edu/upao/pe/ilovetravelfinal/repositories/ChatMessageRepository.java
+++ src/main/java/edu/upao/pe/ilovetravelfinal/repositories/ChatMessageRepository.java
@@ -3,9 +3,11 @@ package edu.upao.pe.ilovetravelfinal.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.upao.pe.ilovetravelfinal.models.ChatMessage;
 import edu.upao.pe.ilovetravelfinal.models.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findAllBySenderOrderBySentAtAsc(User sender);
     List<ChatMessage> findAllByReceiverOrderBySentAtAsc(User receiver);
--- src/main/java/edu/upao/pe/ilovetravelfinal/repositories/UserRepository.java
+++ src/main/java/edu/upao/pe/ilovetravelfinal/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package edu.upao.pe.ilovetravelfinal.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.upao.pe.ilovetravelfinal.models.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByFirstNameAndLastName(String firstName, String lastName);
